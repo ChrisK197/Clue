14a6ed8b7a790743b8841194a2da306bff61f224
Leo Ciccarelli <lciccarelli74@gmail.com>
2020-05-29 08:42:24 -0400

    PLayer class

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,35 @@
 public class Player {
     private Card[] cards;
+    private String name;
+    private int num;
+    private String currentRoom = null;
+    public Player(String name, int num, int numOfCards){
+        this.name = name;
+        this.num = num;
+        cards = new Card[numOfCards];
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCards(Card[] cards) {
+        this.cards = cards;
+    }
+
+    public void setCurrentRoom(String currentRoom) {
+        this.currentRoom = currentRoom;
+    }
 }
