a18fc548f6a5ead715d8a47effdcc8ffcd15a9da
ChrisK197 <chrkal22@bergen.org>
2020-05-29 10:17:38 -0400

    Created pane and scene

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,12 +1,20 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainFile extends Application {
     public void start(Stage ps){
+        Pane mainPane = new Pane();
+
+        //Build onto pane here
+
         ps.setTitle("Clue");
-        //add mainFile here
+        Scene scene = new Scene(mainPane, 1000,1000); //I just put random numbers for now
+        ps.setScene(scene);
+        ps.show();
     }
     public void guess(){
         Stage stage = new Stage();
