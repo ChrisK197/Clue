e52564657b5f1572e01898870d0ffc2c79f741f6
Batman0016 <evacic22@bergen.org>
2020-06-01 08:35:37 -0400

    Added board

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,11 +1,16 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainFile extends Application {
+    private boolean weaponAns = false;
+    private boolean personAns = false;
+
     public void start(Stage ps){
         Pane mainPane = new Pane();
 
@@ -18,8 +23,64 @@ public class MainFile extends Application {
     }
     public void guess(){
         Stage stage = new Stage();
+        stage.setTitle("Guess");
+
+
+        Label labelfirst= new Label("Pick a weapon and a person. The room can only be the one you are in.");
+        Label labelresponse= new Label();
+
+
+        Button submit= new Button("Submit");
+
+        RadioButton knife, gun, candlestick, leadPipe, wrench, rope;
+        knife=new RadioButton("Knife");
+        gun= new RadioButton("Gun");
+        candlestick= new RadioButton("Candlestick");
+        leadPipe= new RadioButton("Lead Pipe");
+        wrench = new RadioButton("Wrench");
+        rope = new RadioButton("Rope");
+
+
+        ToggleGroup weapons= new ToggleGroup();
+
+        knife.setToggleGroup(weapons);
+        gun.setToggleGroup(weapons);
+        candlestick.setToggleGroup(weapons);
+        leadPipe.setToggleGroup(weapons);
+        wrench.setToggleGroup(weapons);
+        rope.setToggleGroup(weapons);
+
+        submit.setDisable(true);
+
+        knife.setOnAction(e -> {
+            weaponChosen(submit); //function
+        } );
+        gun.setOnAction(e-> weaponChosen(submit));
+        candlestick.setOnAction(e-> weaponChosen(submit));
+        leadPipe.setOnAction(e-> weaponChosen(submit));
+        wrench.setOnAction(e-> weaponChosen(submit));
+        rope.setOnAction(e-> weaponChosen(submit));
+
+
+        HBox mainLayout = new HBox(20);
+        VBox weaponLayout= new VBox(5);
+        VBox peopleLayout = new VBox(5); //still need to do people multiple choice
+
+        mainLayout.getChildren().addAll(weaponLayout, peopleLayout);
+        weaponLayout.getChildren().addAll(labelfirst, knife, gun, candlestick, leadPipe, wrench, rope, submit, labelresponse);
+
+        Scene scene1= new Scene(mainLayout, 400, 250);
+        stage.setScene(scene1);
+
+        stage.show();
         //http://www.learningaboutelectronics.com/Articles/Multiple-choice-test-question-in-JavaFX.php
         //add multiple choice bubbles and stuff here
+    }
+    public void weaponChosen(Button submit){
+        weaponAns=true;
+        if(weaponAns==true && personAns==true){
+            submit.setDisable(false);
+        }
     }
 
 }
